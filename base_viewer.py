badaec42bbec35daac752fb0e69ca831dd65f3b3
Ben Meyer-Meisel <59774249+memben@users.noreply.github.com>
2023-06-22 23:28:52 +0200

    Add primitive random point cloud cube and a Camera

---
+++ base_viewer.py
@@ -0,0 +1,109 @@
+import weakref
+import moderngl_window as mglw
+from moderngl_window import activate_context, get_local_window_cls
+from moderngl_window.scene.camera import KeyboardCamera
+from moderngl_window.timers.clock import Timer
+
+
+class CameraWindow(mglw.WindowConfig):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.camera = KeyboardCamera(self.wnd.keys, aspect_ratio=self.wnd.aspect_ratio)
+        self.camera_enabled = True
+        return
+        # inspired by run_window_config
+        # custom init code
+        mglw.setup_basic_logging(self.log_level)
+        parser = mglw.create_parser()
+        self.add_arguments(parser)
+        values = mglw.parse_args(args=kwargs, parser=parser)
+        self.argv = values
+        window_cls = get_local_window_cls(values.window)
+
+        # Calculate window size
+        size = values.size or self.window_size
+        size = int(size[0] * values.size_mult), int(size[1] * values.size_mult)
+
+        # Resolve cursor
+        show_cursor = values.cursor
+        if show_cursor is None:
+            show_cursor = self.cursor
+
+        window = window_cls(
+            title=self.title,
+            size=size,
+            fullscreen=self.fullscreen or values.fullscreen,
+            resizable=values.resizable
+            if values.resizable is not None
+            else self.resizable,
+            gl_version=self.gl_version,
+            aspect_ratio=self.aspect_ratio,
+            vsync=values.vsync if values.vsync is not None else self.vsync,
+            samples=values.samples if values.samples is not None else self.samples,
+            cursor=show_cursor if show_cursor is not None else True,
+            backend=values.backend,
+        )
+        window.print_context_info()
+        activate_context(window=window)
+        self.timer = Timer() # TODO(memben): Figure out: self.timer or Timer()
+        config = self(ctx=window.ctx, wnd=window, timer=self.timer)
+        # Avoid the event assigning in the property setter for now
+        # We want the even assigning to happen in WindowConfig.__init__
+        # so users are free to assign them in their own __init__.
+        window._config = weakref.ref(config)
+
+        # Swap buffers once before staring the main loop.
+        # This can trigged additional resize events reporting
+        # a more accurate buffer size
+        window.swap_buffers()
+        window.set_default_viewport()
+
+        self.timer.start()
+
+    # Custom run code for now
+    def run(self):
+        return ValueError("Note implemented yet")
+        window = mglw.window()
+        self.timer = window.timer
+        while not window.is_closing:
+            current_time, delta = self.timer.next_frame()
+
+            # if config.clear_color is not None:
+            #     window.clear(*config.clear_color)
+
+            # Always bind the window framebuffer before calling render
+            window.use()
+
+            window.render(current_time, delta)
+            if not window.is_closing:
+                window.swap_buffers()
+
+        # _, duration = timer.stop()
+        # window.destroy()
+        # if duration > 0:
+            # logger.info(
+            #     "Duration: {0:.2f}s @ {1:.2f} FPS".format(
+            #         duration, window.frames / duration
+            #     )
+            # )
+
+    def key_event(self, key, action, modifiers):
+        keys = self.wnd.keys
+
+        if self.camera_enabled:
+            self.camera.key_input(key, action, modifiers)
+
+        if action == keys.ACTION_PRESS:
+            if key == keys.C:
+                self.camera_enabled = not self.camera_enabled
+                self.wnd.mouse_exclusivity = self.camera_enabled
+                self.wnd.cursor = not self.camera_enabled
+            if key == keys.SPACE:
+                self.timer.toggle_pause()
+
+    def mouse_position_event(self, x: int, y: int, dx, dy):
+        if self.camera_enabled:
+            self.camera.rot_state(-dx, -dy)
+
+    def resize(self, width: int, height: int):
+        self.camera.projection.update(aspect_ratio=self.wnd.aspect_ratio)
--- point_viewer.py
+++ point_viewer.py
@@ -29,18 +29,20 @@ class PointCloudViewer(CameraWindow):
 
     def render(self, time: float, frametime: float):
         self.ctx.enable_only(moderngl.CULL_FACE | moderngl.DEPTH_TEST | moderngl.PROGRAM_POINT_SIZE)
-
-        rotation = Matrix44.from_eulers((time, time, time), dtype='f4')
-        translation = Matrix44.from_translation((0.0, 0.0, -3.5), dtype='f4')
-        modelview = translation * rotation
-        # projection = self.camera.projection.matrix
-        # camera_matrix = self.camera.matrix
-        # mvp = projection * camera_matrix * modelview
-        self.prog['m_proj'].write(self.camera.projection.matrix)
-        self.prog['m_model'].write(modelview)
-        self.prog['m_camera'].write(self.camera.matrix)
-
+        projection = self.camera.projection.matrix
+        camera_matrix = self.camera.matrix
+        mvp = projection * camera_matrix
+        self.prog['mvp'].write(mvp)
+        self.prog['point_size'].value = 5.0
         self.points.render(self.prog)
+            
+    def key_event(self, key, action, modifiers):
+        super().key_event(key, action, modifiers)
+        if key == self.wnd.keys.SPACE and action == self.wnd.keys.ACTION_PRESS:
+            self.prog = self.load_program('point_debugger.glsl')
+            print("SPACE")
+
+
 
 
 if __name__ == '__main__':
